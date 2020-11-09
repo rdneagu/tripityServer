902130c5472881515ba0e189ccaf9ef23b3bbc64
rdneagu <robertneagu194@gmail.com>
2020-11-09 14:54:43 +0000

    Another attempt to fix

--- __bin__/after-install.sh
+++ __bin__/after-install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 rsync --delete-before --verbose --archive /home/ec2-user/tripity-server-release/ /home/ec2-user/tripity-server > /var/log/deploy.log
 cd /home/ec2-user/tripity-server
-($which npm) install
+$which npm install
--- __bin__/application-start.sh
+++ __bin__/application-start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/tripity-server
-($which pm2) start .
+$which pm2 start .
