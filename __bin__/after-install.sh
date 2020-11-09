fcda2654d74e61cf7be24a268143d113273c2c5e
rdneagu <robertneagu194@gmail.com>
2020-11-09 15:00:07 +0000

    Another attempt to fix deploy issue

--- __bin__/after-install.sh
+++ __bin__/after-install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-rsync --delete-before --verbose --archive /home/ec2-user/tripity-server-release/ /home/ec2-user/tripity-server > /var/log/deploy.log
+rsync --delete-before --verbose --archive /home/ec2-user/tripity-server-release/ /home/ec2-user/tripity-server > /home/ec2-user/deploy.log
 cd /home/ec2-user/tripity-server
-$which npm install
+npm install
