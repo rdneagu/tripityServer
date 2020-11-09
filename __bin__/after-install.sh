606a01e9fadb59e8e032f658b8968ec3776dfb76
rdneagu <robertneagu194@gmail.com>
2020-11-09 19:59:24 +0000

    Fixes rsync in after-install.sh

--- __bin__/after-install.sh
+++ __bin__/after-install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-rsync --remove-source-files --verbose --archive /home/ec2-user/tripity-server-release /home/ec2-user/tripity-server > /home/ec2-user/deploy.log
+rsync --verbose --archive /home/ec2-user/tripity-server-release/ /home/ec2-user/tripity-server/ > /home/ec2-user/deploy.log
+rm -rf /home/ec2-user/tripity-server-release
 cd /home/ec2-user/tripity-server
 npm install
