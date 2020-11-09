a64538c97ffd25f8d9061dda611d7d9321754839
rdneagu <robertneagu194@gmail.com>
2020-11-09 15:04:00 +0000

    Another attempt

--- __bin__/after-install.sh
+++ __bin__/after-install.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
-rsync --delete-before --verbose --archive /home/ec2-user/tripity-server-release/ /home/ec2-user/tripity-server > /home/ec2-user/deploy.log
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+sudo rsync --delete-before --verbose --archive /home/ec2-user/tripity-server-release/ /home/ec2-user/tripity-server > /home/ec2-user/deploy.log
 cd /home/ec2-user/tripity-server
 npm install
