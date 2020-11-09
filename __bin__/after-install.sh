340d69cf5de2fef9cde9f864d33009674e1616c4
rdneagu <robertneagu194@gmail.com>
2020-11-09 14:47:45 +0000

    Fixes npm not found error

--- __bin__/after-install.sh
+++ __bin__/after-install.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 rsync --delete-before --verbose --archive /home/ec2-user/tripity-server-release/ /home/ec2-user/tripity-server > /var/log/deploy.log
 cd /home/ec2-user/tripity-server
 npm install
